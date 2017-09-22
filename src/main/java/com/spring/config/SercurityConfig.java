ae30cc5481ae16df3beca5deb6f415dbf5b83691
wangyang <841234684@qq.com>
2017-09-22 23:29:37 +0800

    create readme.md

--- src/main/java/com/spring/config/SercurityConfig.java
+++ src/main/java/com/spring/config/SercurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 
@@ -46,6 +47,11 @@ public class SercurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
 //                .logoutSuccessUrl("/login")
                 .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resource/static/**");
     }
 
     @Bean
